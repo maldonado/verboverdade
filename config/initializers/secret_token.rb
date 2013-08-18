c8c0d39d0a9256c2d981a50df953b1f180840e90
Everton Maldonado <everton.maldonado@gmail.com>
2013-08-18 00:37:52 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Verboverdade::Application.config.secret_token = '000b276e06397172fa2884262db41055c10291f1186d2950e827644bdf66aba998b9b57ba548b2136c4bd31375fd57c5dc52036fce513f8f09d8302c883b8a4f'
